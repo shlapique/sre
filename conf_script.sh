ea7d8f0b7cf35dc2a9580e3ebedff6acec87518f
shlapique <gordsdima@gmail.com>
2023-03-27 22:02:03 +0300

    added some funcs

--- conf_script.sh
+++ conf_script.sh
@@ -4,6 +4,7 @@
 mount_dir="/home/admini/packages/iso"
 mounted=1
 packages=("net-tools" "bird" "lldp" "openssh-server")
+users=("d.alexeev" "s.ivannikov")
 
 mount_iso() {
     if [ -d "$mount_dir" ];
@@ -42,11 +43,41 @@ installpackages() {
     fi
 }
 
+# adds users with pass and ssh
+#
+# ssh-keygen (to have .pub key for each user)
+# on local machine for each user: 'ssh-copy-key -i /path_to_public_key $user@ip'
 addusers_and_pass() {
+    for user in ${users[@]};
+    do
+        sudo useradd -m "$user" 
+        echo "pls set pass for $user :"
+        sudo passwd $user
+        sudo usermod -aG sudo $user
+        sudo mkdir /home/$user/.ssh
+        sudo chown $user:$user /home/$user/.ssh
+    done
+}
 
+lv_create() {
+    sudo lvcreate -L 30G -n lvVAR vgKVM
+    sudo mkfs.ext4 /dev/vgKVM/lvVAR
+    # to make mountpoint 
+    echo "/dev/vgKVM/lvVAR /var ext4 defaults 0 0" >> /etc/fstab
+    sudo mount /dev/vgKVM/lvVAR /var
+}
+
+swap_create() {
+    sudo fallocate -l 4G /swapfile
+    sudo chmod 0600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 }
 
 # start point of the script
-# mount_iso()
+mount_iso()
 installpackages
 check_for_packages
+addusers_and_pass
+lv_create
